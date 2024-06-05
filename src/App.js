4b2fa135b9d972688887b3b74a372e6cb37b51ee
joserf42 <107516670+joserf42@users.noreply.github.com>
2024-06-04 21:35:57 -0500

    iniciando proyecto

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './componentes/Header';
 
 function App() {
   return (
     <div className="App">
+      
+      <Header />
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola Mundo Con React
         </p>
         <a
           className="App-link"
@@ -15,7 +19,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprende React
         </a>
       </header>
     </div>
---
+++ src/componentes/Header/index.js
@@ -0,0 +1,9 @@
+import "./Encabezado.css"
+
+function Header() {
+    return <header className="header" >
+        <img src="/img/header.png" alt='Header org' />
+    </header>
+}
+
+export default Header
